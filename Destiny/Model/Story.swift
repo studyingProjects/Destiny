871ac17bf1a519aaa402d14f1cc51dd63ad24947
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2023-11-07 21:51:08 +0200

    Step 1. Linking up View elements with Controller

---
+++ Destiny/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  Destiny
+//
+//  Created by Under True on 07/11/2023.
+//
+
+import Foundation
---
+++ Destiny/Model/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  Destiny
+//
+//  Created by Under True on 07/11/2023.
+//
+
+import Foundation
