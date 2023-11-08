2069f9d1ed1a3404a0a174c9bf3f60367618bf8c
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2023-11-08 11:44:19 +0200

    Complete project

--- Destiny/Controller/ViewController.swift
+++ Destiny/Controller/ViewController.swift
@@ -13,12 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        choice1Button.titleLabel?.adjustsFontSizeToFitWidth = true
+        choice2Button.titleLabel?.adjustsFontSizeToFitWidth = true
+        
+        updateUI()
+
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        updateUI()
+    }
+    
+    func updateUI() -> Void {
+        let currentStory = storyBrain.getCurrentStory()
+        storyLabel.text = currentStory.title
+        choice1Button.setTitle(currentStory.choice1, for: .normal)
+        choice2Button.setTitle(currentStory.choice2, for: .normal)
     }
     
 }
